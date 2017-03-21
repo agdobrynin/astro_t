bcee529dec61577fc9f5677db0576ae18c10a047
Alexey Dobrynin <ag.dobrynin@gmail.com>
2017-03-21 12:43:12 +0400

    Change install script

--- install.php
+++ install.php
@@ -3,10 +3,18 @@
 $conf = require_once __DIR__.'/app/conf.php';
 print "\n Create database in ".$conf["db"]."\n-----------\n";
 $db = new SQLite3( $conf["db"] );
+
 $db->exec('
 CREATE TABLE `Calcs` (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`title`	TEXT UNIQUE,
 	`body`	TEXT
+);
+');
+
+$db->exec('
+CREATE TABLE `Calc_results` (
+	`calc_id`	INTEGER NOT NULL,
+	`secret_code`	NUMERIC
 );
 ');
